4338c8db74ea5e6c78c5ef69d85aeff4535a51cd
zacharybklein <zachbklein@gmail.com>
2021-08-17 18:54:30 -0500

    Completed bonus missions

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EquipmentComponent implements OnInit {
 
+  equipment: object[] = [
+    {item: "Habitat dome"},
+    {item: "Drones"},
+    {item: "Food containers"},
+    {item: "Oxygen tanks"}
+  ];
+  itemBeingEdited: object = null;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  add(equipmentName: string) {
+    this.equipment.push({item: equipmentName});
+  }
+
+  remove(type: object) {
+    let index = this.equipment.indexOf(type);
+    this.equipment.splice(index, 1);
+  }  
+
+  edit(item: object) {
+    this.itemBeingEdited = item;
+  }
+
+  save(item: string, type: object) {
+    type['item'] = item;
+    this.itemBeingEdited = null;
   }
 
 }
--- src/app/experiments/experiments.component.ts
+++ src/app/experiments/experiments.component.ts
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExperimentsComponent implements OnInit {
 
+  experiments: object[] = [
+    {task: "Mars soil sample"},
+    {task: "Plant growth in habitat"},
+    {task: "Human bone density"}
+  ];
+  taskBeingEdited: object = null;
+
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  add(experimentName: string) {
+    this.experiments.push({task: experimentName});
+  }
+
+  remove(type: object) {
+    let index = this.experiments.indexOf(type);
+    this.experiments.splice(index, 1);
+  }  
+
+  edit(task: object) {
+    this.taskBeingEdited = task;
+  }
+
+  save(task: string, type: object) {
+    type['task'] = task;
+    this.taskBeingEdited = null;
   }
 
 }
